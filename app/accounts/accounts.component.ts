df5fa875f41328f588f191ca04d79425784540ce
EvanFManuella@gmail.com <evan.manuella@fdmgroup.com>
2019-01-10 22:38:36 -0500

    Auto-generated commit

---
+++ app/accounts/accounts.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Account } from '../classes/account';
+import { AccountsService } from '../service/accounts.service';
+
+@Component({
+  selector: 'app-accounts',
+  templateUrl: './accounts.component.html',
+  styleUrls: ['./accounts.component.css']
+})
+export class AccountsComponent implements OnInit {
+
+  constructor(private accountsService: AccountsService)
+  {
+  }
+
+  accounts : Account[];
+
+  ngOnInit() {
+    this.accountsService.getAccounts().subscribe(
+      (data: Account[]) => this.accounts = data);
+  }
+
+}
